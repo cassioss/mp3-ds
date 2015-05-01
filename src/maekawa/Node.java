e9d4a25e7b8c0fedd6d4a48dc1daaa489672bd45
Cassio Sousa <cassiosantossousa@gmail.com>
2015-04-30 22:57:08 -0500

    Changed to Node

---
+++ src/maekawa/Node.java
@@ -0,0 +1,14 @@
+package maekawa;
+
+import java.util.ArrayList;
+
+/**
+ * @author Cassio
+ * @version 1.0
+ */
+public class Node {
+
+    private int identifier;
+    ArrayList<Integer> subset;
+
+}
